61358a47e682085b305d6f8c82449209e2c501f3
leejanghe <babybye777@gmail.com>
2022-10-18 02:55:25 +0900

    vercel test

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 import styled from "styled-components";
 import ModalForm from "./ModalForm";
 const RedText = styled.div`
+  border: 1px solid black;
   .like {
     color: red;
   }
@@ -26,7 +27,7 @@ function App() {
       title: "사람",
       contents: "사람은 동물일까요?",
       detail: "사람은 동물이 맞아요! 왜냐하면 짐승이거든요!",
-      ilike: 0,
+      ilike: 2,
       isLike: false,
       //   istrue: false,
     },
@@ -35,8 +36,8 @@ function App() {
       title: "원숭이",
       contents: "원숭이는 사람일까요?",
       detail: "원숭이는 동물이예요!@#!@!@!!",
-      ilike: 0,
-      isLike: false,
+      ilike: 3,
+      isLike: true,
       //   istrue: false,
     },
     {
@@ -46,6 +47,22 @@ function App() {
       detail: "할말이 없어요~@~@~@@@낄끼루",
       ilike: 0,
       isLike: false,
+    },
+    {
+      id: 3,
+      title: "저아여기능",
+      contents: "더미데꾸루루.",
+      detail: "할키키@낄끼루",
+      ilike: 0,
+      isLike: false,
+    },
+    {
+      id: 4,
+      title: "더미더미",
+      contents: "더미데이터입니다.",
+      detail: "할쿠루무끼루",
+      ilike: 1,
+      isLike: true,
     },
   ]);
 
@@ -84,7 +101,7 @@ function App() {
                 className={a.isLike ? "like" : undefined}
                 onClick={() => plusBtn(i)}
               >
-                {a.ilike}
+                좋아요{a.ilike}
               </p>
               <p onClick={() => handleClick(a.id)}>자세히 보기</p>
               {modalVisbleId === a.id && showModal === true ? (
