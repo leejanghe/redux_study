7b19866f2a179252378d95256859d72eac9d52d8
leejanghe <babybye777@gmail.com>
2022-10-20 22:30:29 +0900

    css fix

--- client/src/App.js
+++ client/src/App.js
@@ -13,9 +13,20 @@ const RedText = styled.div`
 `;
 
 const TextStyled = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  -webkit-transform: translate(-50%, -50%);
+  -moz-transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  -o-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
   text-align: center;
   > h1 {
-    font-size: 24px;
+    font-size: 30px;
     font-weight: 700;
   }
   > button {
